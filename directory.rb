8bec4fdb5666b96027b55083cd33158c8209fedc
kierangoodacre <k.goodacre@hotmail.co.uk>
2014-12-05 09:46:34 +0000

    added array

--- directory.rb
+++ directory.rb
@@ -1,13 +1,24 @@
-student_count = 9
+students = [
+"Kieran Goodacre",
+"The Joker",
+"Batman",
+"Spiderman",
+"Superman",
+"El Goucho",
+"Iceman",
+"Fireman",
+"Donmar"
+]
 puts "The students of December 2014 cohort are..."
 puts "----------------"
-puts "Kieran Goodacre"
-puts "The Joker"
-puts "Batman"
-puts "Spiderman"
-puts "Superman"
-puts "El Goucho"
-puts "Iceman"
-puts "Fireman"
-puts "Donmar"
-print "Overall we have #{student_count} great students"
+puts students[0]
+puts students[1]
+puts students[2]
+puts students[3]
+puts students[4]
+puts students[5]
+puts students[6]
+puts students[7]
+puts students[8]
+puts students[9]
+puts "Overall we have #{students.length} great students"
