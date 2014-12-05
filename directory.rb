60e01e3f0029979ba9c3f2a8865266e93836c686
kierangoodacre <k.goodacre@hotmail.co.uk>
2014-12-05 10:01:10 +0000

    added methods

--- directory.rb
+++ directory.rb
@@ -9,9 +9,22 @@ students = [
 "Fireman",
 "Donmar"
 ]
+
+def print_header
 puts "The students of December 2014 cohort are..."
 puts "----------------"
-students.each do |student|
-	puts student
 end
-puts "Overall we have #{students.length} great students"
+
+def print(names)
+	names.each do |name|
+		puts name
+	end
+end
+
+def print_footer(names)
+puts "Overall we have #{names.length} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
