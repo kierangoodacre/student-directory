bcbf463e02c3eff172e3b8eeabd5774fad255523
kierangoodacre <k.goodacre@hotmail.co.uk>
2014-12-05 09:47:26 +0000

    interated array

--- directory.rb
+++ directory.rb
@@ -11,14 +11,7 @@ students = [
 ]
 puts "The students of December 2014 cohort are..."
 puts "----------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
+students.each do |student|
+	puts student
+end
 puts "Overall we have #{students.length} great students"
