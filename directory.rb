e83c80ddeadecab75153c8caa5a230ce1f4aa8e0
Kieran Goodacre <kierangoodacre@Kierans-MacBook-Pro.local>
2014-12-08 13:10:26 +0000

    user input name

--- directory.rb
+++ directory.rb
@@ -1,14 +1,15 @@
-students = [
-{:name => "Kieran Goodacre", :cohort => :December},
-{:name => "The Joker", :cohort => :December},
-{:name => "Batman", :cohort => :December},
-{:name => "Spiderman", :cohort => :December},
-{:name => "Superman", :cohort => :December},
-{:name => "El Goucho", :cohort => :December},
-{:name => "Iceman", :cohort => :December},
-{:name => "Fireman", :cohort => :December},
-{:name => "Donmar", :cohort => :December},
-]
+def input_students
+	puts "Please enter name of the students"
+	puts "To finish press enter twice"
+	students = []
+	name = gets.chomp
+	while !name.empty? do
+		students << {:name => name, :cohort => :december}
+		puts "Now we have #{students.length} students"
+		name = gets.chomp
+	end
+	students
+end
 
 def print_header
 puts "The students of December 2014 cohort are..."
@@ -25,6 +26,7 @@ def print_footer(names)
 puts "Overall we have #{names.length} great students"
 end
 
+students = input_students
 print_header
 print(students)
 print_footer(students)
